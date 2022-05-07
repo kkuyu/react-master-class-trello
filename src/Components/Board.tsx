f12b5a73b33cee13633bf672747e36a0af499d60
kkuyu <devils67@naver.com>
2022-05-07 12:02:28 +0900

    Final Styles

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -6,10 +6,11 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 300px;
-  padding: 10px 10px 20px;
+  padding-top: 10px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
   min-height: 300px;
+  overflow: hidden;
 `;
 
 const Title = styled.h2`
@@ -25,8 +26,9 @@ interface IAreaProps {
 }
 
 const Area = styled.div<IAreaProps>`
-  background-color: ${(props) => (props.isDraggingOver ? "pink" : props.isDraggingFromThis ? "red" : "blue")};
+  background-color: ${(props) => (props.isDraggingOver ? "#dfe6e9" : props.isDraggingFromThis ? "#b2bec3" : "transparent")};
   flex-grow: 1;
+  padding: 20px;
   transition: background-color 0.3s ease-in-out;
 `;
 
